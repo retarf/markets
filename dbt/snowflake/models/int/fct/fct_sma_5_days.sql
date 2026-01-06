fb6ee704349ec20607b19ad7fdf7815a8705cb66
Łukasz Długajczyk <lukasz.dlugajczyk@stxnext.pl>
2026-01-06 16:37:26 +0100

    last_close column has been addded.

--- dbt/snowflake/models/int/fct/fct_sma_5_days.sql
+++ dbt/snowflake/models/int/fct/fct_sma_5_days.sql
@@ -1,2 +1,13 @@
+{{ config(
+    materialized='incremental',
+    unique_key=['ticker', 'trading_date'],
+    incremental_strategy='merge'
+) }}
 with close_data as (select trading_date, ticker, close from {{ ref('stg_stock_data') }})
-    select trading_date, ticker, close, avg(close) over (partition by ticker order by trading_date rows between 4 preceding and current row) as sma from close_data
+    select
+        trading_date,
+        ticker,
+        close,
+        avg(close) over (partition by ticker order by trading_date rows between 4 preceding and current row) as sma,
+        last_value(close) over (partition by ticker order by trading_date rows between unbounded preceding and 1 preceding) as last_close
+    from close_data
