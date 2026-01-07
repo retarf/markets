88e1d435df020dbdf88715e9d65748d61fff9995
Łukasz Długajczyk <lukasz.dlugajczyk@stxnext.pl>
2026-01-07 15:44:39 +0100

    last_sma has been added.

--- dbt/snowflake/models/int/fct/fct_sma_5_days.sql
+++ dbt/snowflake/models/int/fct/fct_sma_5_days.sql
@@ -3,11 +3,20 @@
     unique_key=['ticker', 'trading_date'],
     incremental_strategy='merge'
 ) }}
-with close_data as (select trading_date, ticker, close from {{ ref('stg_stock_data') }})
+with close_data as (select trading_date, ticker, close from {{ ref('stg_stock_data') }}),
+calculations as (
     select
         trading_date,
         ticker,
         close,
         avg(close) over (partition by ticker order by trading_date rows between 4 preceding and current row) as sma,
-        last_value(close) over (partition by ticker order by trading_date rows between unbounded preceding and 1 preceding) as last_close
     from close_data
+)
+select
+    trading_date,
+    ticker,
+    close,
+    sma,
+    LAG(close) over (partition by ticker order by trading_date) as last_close,
+    LAG(sma) over (partition by ticker order by trading_date) as last_sma
+from calculations
