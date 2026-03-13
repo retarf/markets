54b2be9a4656469c2a5100c5c13e49173cf1c670
Łukasz Długajczyk <lukasz.dlugajczyk@stxnext.pl>
2026-03-13 12:22:16 +0100

    STOCK_DATA::PUSH_DATA: Prepare metastore operations

---
+++ src/stock_data/push_data/metastore.py
@@ -0,0 +1,44 @@
+from datetime import date
+
+from pyspark.sql.functions import col
+
+from warehouse.snowflake.session import get_spark_session
+from warehouse.snowflake.table import save_table, load_table
+
+from stock_data import APP_NAME, Column
+
+
+
+METASTORE_SCHEMA = "METASTORE"
+LAST_DATA_DATE_TABLE = "LAST_DATA_DATE_TABLE"
+TABLE_PATH = f"MARKETS.{METASTORE_SCHEMA}.{LAST_DATA_DATE_TABLE}"
+
+
+spark = get_spark_session(APP_NAME)
+
+
+schema = StructType(
+    [
+        StructField(Column.ticker, StringType(), False),
+        StructField(Column.date, DateType(), False),
+    ]
+)
+
+
+def save_last_trading_date(ticker: str, last_date: date) -> None:
+    '''
+        Saves last trading date for given ticker in metastore.
+    '''
+    last_date_df = spark.createDataFrame([(ticker, last_date)], schema)
+    #TODO: MARKETS db should be replaced by {APP_NAME}
+    save_table(last_date_df, table_name=TABLE_PATH)
+
+
+def load_last_trading_date(ticker: str) -> date | None:
+    '''
+        loads last trading date for given ticker from metastore. 
+        Returns None if no data exists.
+    '''
+    last_date_df = load_table(spark=TABLE_PATH)
+    row = last_date_df.filter( col(Column.ticker) == ticker ).orderBy( col(Column.date).desc() ).first()
+    return row[Column.date] if row else None
